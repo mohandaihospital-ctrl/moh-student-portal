9f31cddf93c84e2792ed98e61fbf962a91a18dfc
Aman Saini <amansaini2537@gmail.com>
2026-06-02 13:46:56 +0530

    update

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -43,24 +43,7 @@ router.post(
   resetPassword
 );
 
-router.get("/email-test", async (req, res) => {
-  try {
-    await sendEmail(
-      "yourpersonalemail@gmail.com",
-      "Test Email",
-      "<h1>Test</h1>"
-    );
 
-    res.json({
-      success: true,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: err.message,
-    });
-  }
-});
 
 router.get("/me", protect, getMe);
 
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,24 +5,52 @@ const sendEmail = async (
   subject,
   html
 ) => {
-  const transporter =
-    nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      family: 4,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject,
-    html,
-  });
+  try {
+
+    console.log(
+      "Sending email to:",
+      email
+    );
+
+    const transporter =
+      nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        family: 4,
+
+        auth: {
+          user:
+            process.env.EMAIL_USER,
+          pass:
+            process.env.EMAIL_PASS,
+        },
+      });
+
+    const info =
+      await transporter.sendMail({
+        from:
+          process.env.EMAIL_USER,
+        to: email,
+        subject,
+        html,
+      });
+
+    console.log(
+      "EMAIL SENT:",
+      info.messageId
+    );
+
+  } catch (error) {
+
+    console.log(
+      "EMAIL ERROR:",
+      error
+    );
+
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
