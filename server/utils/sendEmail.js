1a3404c4405bfb538937ff2a0ccb861fe4f89268
Aman Saini <amansaini2537@gmail.com>
2026-06-02 13:16:53 +0530

    update

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,16 +6,15 @@ const sendEmail = async (
   html
 ) => {
   const transporter =
-    nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      family: 4,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
